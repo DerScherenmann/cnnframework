f173363da6ee010431ff1ca62634661fccc21044
Robert Nickel <rnickel2000@gmail.com>
2022-01-18 21:56:49 +0100

    Essentially nothing

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #define INPUT_DIMENSIONS 3
 
 #include "convnetwork.h"
+#include <iostream>
 
 int main(int argc, char** argv) {
     
@@ -16,14 +17,15 @@ int main(int argc, char** argv) {
     size_t num_filter_size = 4;
     size_t num_pool_size = 2;
     size_t num_zero_padding = 1;
-    
+
     // WARNING knn library still bugged, can only be trained once TODO fix
     Convolutional conv = Convolutional({28,28,3},{10,1},{1,2},num_repeats,num_filters,num_filter_size,num_pool_size,num_zero_padding);
     
     conv.run_tests();
-    while(getchar() != 0){
-        
-    }
+
+    std::cout << "Exited normally" << std::endl;
+
+    getchar();
 
     return 0;
 }
