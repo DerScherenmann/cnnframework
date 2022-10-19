ccf0fe35b05aa5c1c5e837f1cbfc08052c4fe9b4
Robert Nickel <rnickel2000@gmail.com>
2022-10-19 11:02:55 +0200

    Update .gitignore

--- cmake-build-release/Boost_tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derscherenmann/CLionProjects/cnnframework/Boost_tests
-# Build directory: /home/derscherenmann/CLionProjects/cnnframework/cmake-build-release/Boost_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derscherenmann/CLionProjects/cnnframework
-# Build directory: /home/derscherenmann/CLionProjects/cnnframework/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("Boost_tests")
